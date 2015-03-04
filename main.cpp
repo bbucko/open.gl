af9437c27f40be60f00e7afe65516a75c8038b4c
Blazej Bucko <blazej.bucko@gmail.com>
2015-03-04 21:35:38 +0100

    Initial version OpenGL + SDL

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <GL/glew.h>
+//#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <SDL.h>
+
+int main() {
+//    createWindow(title, width, height);
+//    createOpenGLContext(settings);
+//
+//    while (windowOpen) {
+//        while (event = newEvent())
+//            handleEvent(event);
+//
+//        updateScene();
+//
+//        drawGraphics();
+//        presentGraphics();
+//    }
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_Window *window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+    printf("%u\n", vertexBuffer);
+
+    SDL_Event windowEvent;
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (windowEvent.type == SDL_QUIT) break;
+        }
+
+        SDL_GL_SwapWindow(window);
+    }
+
+
+    SDL_GL_DeleteContext(context);
+    SDL_Quit();
+
+    return 0;
+}
